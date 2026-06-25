f4e94871973fd3e3183be08293114a332abe4d7e
samyguti089-art <samyguti089@gmail.com>
2026-06-24 21:56:07 -0500

    Fix import path for supabaseClient in CashRegister

--- src/CashRegister.jsx
+++ src/CashRegister.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from './supabaseClient';
 import { FaMoneyBillWave, FaBoxes, FaCheckCircle, FaExclamationTriangle, FaSave } from 'react-icons/fa';
 import './CashRegister.css';
 
