0313d45b6c9d1a94230eea779a39ed2e0e516644
samyguti089-art <samyguti089@gmail.com>
2026-06-26 23:48:10 -0500

    Update formatPrice.js

--- src/utils/formatPrice.js
+++ src/utils/formatPrice.js
@@ -1,1 +1,5 @@
-
+// src/utils/format.js
+export const formatPrice = (value) => {
+  if (value === undefined || value === null) return '$0';
+  return `$${Number(value).toLocaleString('es-CO')}`;
+};
